7a3358186b5e42626726000baf74c4ba3005b78f
David Yang <didalfn88@gmail.com>
2020-03-13 17:50:37 +0900

    #6 User Authentication(Passport, Social Login)

--- routes.js
+++ routes.js
@@ -12,6 +12,7 @@ const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const ME = "/me";
 
 // Videos
 
@@ -20,6 +21,16 @@ const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
+// Facebook
+
+const FB = "/auth/facebook";
+const FB_CALLBACK = "/auth/facebook/callback";
 
 const routes = {
   home: HOME,
@@ -59,7 +70,12 @@ const routes = {
     } else {
       return DELETE_VIDEO;
     }
-  }
+  },
+  me: ME,
+  gitHub: GITHUB,
+  githubCallback: GITHUB_CALLBACK,
+  facebook: FB,
+  facebookCallback: FB_CALLBACK
 };
 
 export default routes;
