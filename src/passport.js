ef8806b2f568d01794885e9b7eec188d55cf6778
David Yang <didalfn88@gmail.com>
2020-03-31 18:06:58 +0900

    #11.5 - #11.6 Deploying to Heroku

--- src/passport.js
+++ src/passport.js
@@ -14,7 +14,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: process.env.PRODUCTION
+        ? `https://tranquil-springs-85545.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
@@ -25,7 +27,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://f7e3a6f1.ngrok.io${routes.facebookCallback}`,
+      callbackURL: `https://tranquil-springs-85545.herokuapp.com${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"]
     },
